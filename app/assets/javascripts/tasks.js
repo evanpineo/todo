4425acd7d225f6928fbab6240bb42df8c98949e6
Evan <evan.pineo@gmail.com>
2017-08-14 23:49:25 +0000

    css updates

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,8 +1,9 @@
 $(function() {
 		function taskHtml(task) {
 			var checkedStatus = task.done? "checked" : "";
+			var liClass = task.done? "completed" : "";
 			var liElement = '' +
-        '<li>' +
+        '<li id="listItem-' + task.id +'" class="' + liClass + '">' +
           '<div class="view">' +
             '<input class="toggle" type="checkbox" data-id="' + task.id + '" ' + checkedStatus + '>' +
             '<label>' + task.title + '</label>' +
@@ -17,6 +18,11 @@ $(function() {
 			
 			$.post("/tasks/" + itemId, {
 				_method: "PUT", task: { done: doneValue }
+			}).success(function(data) {
+				var liHtml = taskHtml(data);
+				var $li = $("#listItem-" + data.id);
+				$li.replaceWith(liHtml);
+				$('.toggle').change(toggleTask);
 			});
 		}
 		
